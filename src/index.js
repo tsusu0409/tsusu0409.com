03851cd5f1db4aade3155d2d80e2703ae085227c
tsusu0409 <tsubasa.kawagishi@gamil.com>
2025-05-09 01:57:20 +0900

    to build ver2.0

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App.js';
-import './styles/index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(React.createElement(App));
