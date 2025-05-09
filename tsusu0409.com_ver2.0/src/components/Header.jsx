f623cb4c8408fe0bf1454a7cf888f2f36f12da47
tsusu0409 <tsubasa.kawagishi@gmail.com>
2025-05-09 11:03:49 +0900

    update header

--- tsusu0409.com_ver2.0/src/components/Header.jsx
+++ tsusu0409.com_ver2.0/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import './Header.css';
 import profIcon from '../assets/images/prof-icon.jpg';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
     return (
         <div>
-            <div className='container'>
+            <div className="container">
                 <div className="header-profs">
                     <div className="prof-icon">
                         <img src={profIcon} alt="プロフィールアイコン" />
@@ -14,7 +19,19 @@ function Header() {
                         <p className="real">Tsubasa KAWAGISHI</p>
                     </div>
                 </div>
-                    <div className="header-links">
+                <div className="header-links">
+                    <a href="https://x.com/tsusu0409" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faXTwitter} className="icon" />
+                    </a>
+                    <a href="https://youtube.com/@tsusu0409" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faYoutube} className="icon" />
+                    </a>
+                    <a href="https://github.com/tsusu0409" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faGithub} className="icon" />
+                    </a>
+                    <a href="mailto:tsusu0409@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faEnvelope} className="icon" />
+                    </a>
                 </div>
             </div>
         </div>
