b657e9da69fca6c20c9cc099b308a2979227b80d
tsusu0409 <tsubasa.kawagishi@gamil.com>
2025-07-22 00:54:51 +0900

    add rss method

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,24 +1,22 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-
-const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const blog = (await getCollection('blog'))
-    .sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
-    .slice(0, 10);
+  const allBlogPosts = import.meta.glob('./blog/*.{md,mdx}', { eager: true });
+
+  const items = Object.values(allBlogPosts).map((post) => {
+    return {
+      title: post.frontmatter.title,
+      pubDate: post.frontmatter.date,
+      description: post.frontmatter.description,
+      link: post.url,
+      customData: `<thumb>${post.frontmatter.thumb}</thumb>`,
+    };
+  });
+
   return rss({
     title: 'Tsusuのブログ',
     description: 'tsusu0409.comの最新投稿',
     site: context.site,
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.date,
-      description: post.data.description,
-      link: `/blog/${post.slug}/`,
-      content: sanitizeHtml(parser.render(post.body)),
-    })),
+    items: items,
   });
 }
